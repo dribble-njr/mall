ff4d157fdcbfc8d56a09a21a6a1026e8e91346fa
Stephen-wzw <wzw15292257101@163.com>
2021-03-22 13:41:01 +0800

    配置路由

---
+++ src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const Home = () => import ('views/home/Home')
+const Category = () => import ('views/category/Category')
+const Cart = () => import ('views/cart/Cart')
+const Profile = () => import ('views/profile/Profile')
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    component: Home
+  },
+  {
+    path: '/category',
+    component: Category
+  },
+  {
+    path: '/cart',
+    component: Cart
+  },
+  {
+    path: '/profile',
+    component: Profile
+  }
+]
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
+
+export default router
