2bc838082c866fd6f40c0d05cc8260781116b480
Stephen-wzw <wzw15292257101@163.com>
2021-03-22 18:04:43 +0800

    使用tabbar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app">
+    <router-view></router-view>
+    <main-tab-bar></main-tab-bar>
   </div>
 </template>
 
 <script>
+import MainTabBar from 'components/content/mainTabBar/MainTabBar'
 
 export default {
   name: 'App',
   components: {
+    MainTabBar
   }
 }
 </script>
