4b41bc55df4964e3750d712105ce03240e51c3e3
Stephen-wzw <wzw15292257101@163.com>
2021-03-22 20:44:42 +0800

    首页导航栏封装

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,25 @@
 <template>
   <div>
-    首页
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
   </div>
 </template>
 
 <script>
-export default {
+import NavBar from 'components/common/navbar/NavBar'
 
+export default {
+  name: 'Home',
+  components: {
+    NavBar
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+.home-nav {
+  background-color: var(--color-tint);
+  color: #ffffff;
+}
 </style>
