bdaaba36752b6eb5934efa801d5d82b3406afabb
Stephen-wzw <wzw15292257101@163.com>
2021-03-24 09:56:40 +0800

    首页轮播图

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,17 +3,21 @@
     <nav-bar class="home-nav">
       <div slot="center">购物街</div>
     </nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
   </div>
 </template>
 
 <script>
-import NavBar from "components/common/navbar/NavBar";
-import { getHomeMultiData } from "network/home";
+import NavBar from "components/common/navbar/NavBar"
+import HomeSwiper from "./childComps/HomeSwiper";
+
+import { getHomeMultiData } from "network/home"
 
 export default {
   name: "Home",
   components: {
     NavBar,
+    HomeSwiper
   },
   data() {
     return {
@@ -24,6 +28,7 @@ export default {
   created() {
     // 1.请求多个数据
     getHomeMultiData().then((res) => {
+      console.log(res);
       this.banners = res.data.banner.list;
       this.recommends = res.data.recommend.list;
     });
