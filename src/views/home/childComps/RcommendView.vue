78bfcb266a3f8cb0dbbf88f31342487f6a6653ef
Stephen-wzw <wzw15292257101@163.com>
2021-03-24 14:25:54 +0800

    RecommendView封装

---
+++ src/views/home/childComps/RcommendView.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="recommend">
+    <div v-for="(item, index) in recommends" :key="index" class="recommend-item">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+        <div>{{ item.title }}</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RecommendView',
+  props: {
+    recommends: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+}
+</script>
+
+<style scoped>
+.recommend {
+  display: flex;
+  padding: 10px 0 20px;
+
+  text-align: center;
+  font-size: 14px;
+
+  border-bottom: 10px solid #eeeeee;
+}
+
+.recommend-item {
+  flex: 1;
+}
+
+.recommend-item img {
+  width: 70px;
+  height: 70px;
+  margin-bottom: 10px;
+}
+</style>
