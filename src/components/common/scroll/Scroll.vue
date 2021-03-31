72fcf71cdbf9cd972acf4b43567cf04fb87f6902
Stephen-wzw <wzw15292257101@163.com>
2021-03-31 10:38:35 +0800

    better-scroll封装

---
+++ src/components/common/scroll/Scroll.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="wrapper" ref="wrapper">
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import BScroll from "better-scroll";
+
+export default {
+  data() {
+    return {
+      // scroll: null,
+    };
+  },
+  mounted() {
+    this.initScroll();
+  },
+  methods: {
+    initScroll() {
+      this.scroll = new BScroll(this.$refs.wrapper, {
+        click: true,
+      });
+
+      this.scroll.on("beforeScrollStart", () => {
+        this.scroll.refresh();
+      });
+
+      console.log(this.scroll);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.wrapper {
+  overflow: hidden;
+}
+</style>
