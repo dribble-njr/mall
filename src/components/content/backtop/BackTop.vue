e0d9e3b585d3a89f88fbeb128825c88ab55cb61e
Stephen-wzw <wzw15292257101@163.com>
2021-03-31 13:11:31 +0800

    回到顶部

---
+++ src/components/content/backtop/BackTop.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="back-top">
+    <img src="~assets/img/common/top.png" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.back-top {
+  position: fixed;
+  bottom: 54px;
+  right: 10px;
+}
+
+.back-top img {
+  width: 43px;
+  height: 43px;
+}
+</style>
