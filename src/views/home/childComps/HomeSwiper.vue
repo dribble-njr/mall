0ffe710ceef20304932efeef995d7d112104e2c6
Stephen-wzw <wzw15292257101@163.com>
2021-04-01 16:09:17 +0800

    监听轮播图加载完成

--- src/views/home/childComps/HomeSwiper.vue
+++ src/views/home/childComps/HomeSwiper.vue
@@ -2,7 +2,7 @@
   <swiper>
     <swiper-item v-for="(item, index) in banners" :key="index">
       <a :href="item.link">
-        <img :src="item.image" alt=""/>
+        <img :src="item.image" alt="" @load="imageLoad" />
       </a>
     </swiper-item>
   </swiper>
@@ -21,10 +21,23 @@ export default {
       }
     }
   },
+  data() {
+    return {
+      isLoad: false,
+    }
+  },
   components: {
     Swiper,
     SwiperItem
   },
+  methods: {
+    imageLoad() {
+      if (!this.isLoad) {
+        this.$emit('swipperImageLoad');
+        this.isLoad = true;
+      }
+    }
+  }
 }
 </script>
 
