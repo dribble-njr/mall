d54f11aed8c2df9019bb8964bc723e378f07db42
Stephen-wzw <wzw15292257101@163.com>
2021-04-01 19:53:16 +0800

    详情页导航栏封装

---
+++ src/views/detail/Detail.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="detail">
+    <detail-nav-bar/>
+  </div>
+</template>
+
+<script>
+import DetailNavBar from './childComps/DetailNavBar'
+
+export default {
+  components: {
+    DetailNavBar,
+  },
+  data() {
+    return {
+      iid: null,
+    }
+  },
+  created() {
+    this.iid = this.$route.query.iid
+  }
+}
+</script>
+
+<style scoped>
+/* .detail {
+  overflow: hidden;
+} */
+</style>
---
+++ src/views/detail/childComps/DetailNavBar.vue
@@ -0,0 +1,55 @@
+<template>
+  <nav-bar>
+    <div slot="left" class="back" @click="backClick">
+      <img src="~assets/img/common/back.svg" alt="">
+    </div>
+    <div slot="center" class="title">
+        <div v-for="(item, index) in titles" :key="index" class="title-item" @click="titleClick(index)" :class="{ active: index === currentIndex }">
+          {{ item }}
+        </div>
+    </div>
+  </nav-bar>
+</template>
+
+<script>
+import NavBar from 'components/common/navbar/NavBar'
+
+export default {
+  components: {
+    NavBar,
+  },
+  data() {
+    return {
+      titles: ['商品', '参数', '评论', '推荐'],
+      currentIndex: 0,
+    }
+  },
+  methods: {
+    backClick() {
+      this.$router.back()
+    },
+    titleClick(index) {
+      this.currentIndex = index
+    }
+  }
+}
+</script>
+
+<style scoped>
+.title {
+  display: flex;
+  font-size: 13px;
+}
+
+.title-item {
+  flex: 1;
+}
+
+.back img {
+  vertical-align: middle;
+}
+
+.active {
+  color: var(--color-high-text);
+}
+</style>
