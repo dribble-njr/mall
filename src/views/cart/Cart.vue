ceb9a5b536c9fcd9c771f0e28df2e9eabbd59841
Stephen-wzw <wzw15292257101@163.com>
2021-04-07 15:06:20 +0800

    ss

--- src/views/cart/Cart.vue
+++ src/views/cart/Cart.vue
@@ -3,22 +3,27 @@
     <nav-bar class="nav-bar">
       <div slot="center">购物车({{ length }})</div>
     </nav-bar>
+    <cart-list/>
   </div>
 </template>
 
 <script>
 import NavBar from 'components/common/navbar/NavBar'
 
+import CartList from './childComps/CartList';
+
 import { mapGetters } from 'vuex';
 export default {
   components: {
     NavBar,
+
+    CartList,
   },
   computed: {
     // 两种语法
     // ...mapGetters(['cartLength', 'cartList'])
     ...mapGetters({
-      length: 'cartLenght',
+      length: 'cartLength',
       list: 'cartList'
     })
   }
@@ -26,6 +31,10 @@ export default {
 </script>
 
 <style scoped>
+.cart {
+  height: 100vh;
+}
+
 .nav-bar {
   background-color: var(--color-tint);
   color: #fff;
