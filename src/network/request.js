2ccdea73887049ac35ff53f5228fffd3e9870da1
Stephen-wzw <wzw15292257101@163.com>
2022-03-23 10:47:51 +0800

    更新接口

--- src/network/request.js
+++ src/network/request.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export function request (config) {
   const instance = axios.create({
-    baseURL: 'http://152.136.185.210:7878/api/m5',
+    baseURL: 'http://152.136.185.210:7878/api/hy66',
     timeout: 10000
   })
 
