cbe5d7aeca1615e4721cd9774c5ef975d3f230ae
Stephen-wzw <wzw15292257101@163.com>
2022-03-29 12:40:55 +0800

    最终版

--- src/router/index.js
+++ src/router/index.js
@@ -4,9 +4,7 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const Home = () => import ('views/home/Home')
-const Category = () => import ('views/category/Category')
 const Cart = () => import ('views/cart/Cart')
-const Profile = () => import ('views/profile/Profile')
 const Detail = () => import ('views/detail/Detail')
 
 const routes = [
@@ -19,16 +17,8 @@ const routes = [
     component: Home
   },
   {
-    path: '/category',
-    component: Category
-  },
-  {
     path: '/cart',
     component: Cart
-  },
-  {
-    path: '/profile',
-    component: Profile
   },
   {
     path: '/detail',
